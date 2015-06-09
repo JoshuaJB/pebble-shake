faf164a460ab2d92847489170aed3e57ad8d62e6
Joshua <JoshuaJB@users.noreply.github.com>
2015-06-09 13:33:00 -0400

    Cleanup and fix memory leak

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ static uint32_t dataCacheSize = 100;
 static uint32_t dataCacheIdx = 0;
 static uint16_t * dataCache;
 static const AccelSamplingRate SAMPLE_RATE = ACCEL_SAMPLING_100HZ;
-static uint16_t highScore;
+static uint16_t highscore;
 enum states {
   RECORDING,
   FINISHED,
@@ -37,7 +37,7 @@ static void start(ClickRecognizerRef recognizer, void *context) {
   // Register acceleration event handler with a 25 sample buffer
   accel_data_service_subscribe(25, cache_accel);
   // Display the pre-run message
-  text_layer_set_text(text_layer, "Recording...\n\nShake, shake, shake!\n\n(press any button to finish)");
+  text_layer_set_text(text_layer, "Recording...\n\nShake away!\n\n(press any button to finish)");
 }
 
 // Analyse and dislay data
@@ -48,22 +48,23 @@ static void finish(ClickRecognizerRef recognizer, void *context) {
   state = FINISHED;
   // De-register acceleration event handler
   accel_data_service_unsubscribe();
-  // Analyse and display
+  // Find maximimum accelerometer reading
   uint16_t max = dataCache[0];
   for (unsigned int i = 1; i < dataCacheIdx; i++)
     if (dataCache[i] > max)
       max = dataCache[i];
-  if (max > highScore) {
-    static char score_text[83];
-    snprintf(score_text, 83, "Congratulations! You have a new high score of %d.\n\nThe old high score was %d.", max, highScore);
+  // Display appropriate message
+  if (max > highscore) {
+    static char score_text[117];
+    snprintf(score_text, 117, "Congratulations! You have a new high score of %d.\n\nThe old high score was %d.\n\nTo play again, press any button.", max, highscore);
     text_layer_set_text(text_layer, score_text);
     // Save new high score
-    highScore = max;
-    persist_write_int(0, highScore);
+    highscore = max;
+    persist_write_int(0, highscore);
   }
   else {
-    static char score_text[62];
-    snprintf(score_text, 62, "Good try. Your score was %d and the high score is %d.", max, highScore);
+    static char score_text[96];
+    snprintf(score_text, 96, "Good try. Your score was %d and the high score is %d.\n\nTo play again, press any button.", max, highscore);
     text_layer_set_text(text_layer, score_text);
   }
 }
@@ -105,12 +106,12 @@ void click_config_provider(Window *window) {
 static void main_window_load(Window *window) {
   // Setup the text layer
   text_layer = text_layer_create(layer_get_bounds(window_get_root_layer(my_window)));
-  text_layer_set_text(text_layer, "Welcome! To get started, press any button.");
+  text_layer_set_text(text_layer, "Welcome!\n\nTo get started, press any button.");
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_layer));
   // Set Accelerometer to sample rate
   accel_service_set_sampling_rate(SAMPLE_RATE);
   // Load initial high score
-  highScore = persist_read_int(0);
+  highscore = persist_read_int(0);
 }
 
 void handle_init(void) {
@@ -124,6 +125,7 @@ void handle_init(void) {
 void handle_deinit(void) {
   text_layer_destroy(text_layer);
   window_destroy(my_window);
+  free(dataCache);
 }
 
 int main(void) {
